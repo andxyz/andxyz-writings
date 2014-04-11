55cd63e3c22b51307525d12778903b48b52a99ed
Andrew Stevens <andxyz.stevens@gmail.com>
2014-04-11 16:09:32 -0400

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-git push origin andxyz > /dev/null 2>&1 && echo "updated andxyz repo"
-hub push github andxyz > /dev/null 2>&1 && echo "updated github repo"
+set -e
 
-bundle exec rake clean > /dev/null 2>&1 && bundle exec rake generate > /dev/null 2>&1 && bundle exec rake deploy > /dev/null 2>&1 && echo "deployed to http://andxyz.com"
+git push origin andxyz > /dev/null 2>&1 && 
+echo "updated deployer@deployer.andxyz.com:andxyz-writings.git repo"
+
+git push github andxyz > /dev/null 2>&1 &&
+echo "updated git@github.com-andxyz:andxyz/andxyz-writings.git repo"
+
+bundle exec rake clean > /dev/null 2>&1 && 
+bundle exec rake generate > /dev/null 2>&1 && 
+bundle exec rake deploy > /dev/null 2>&1 && 
+echo "deployed to http://andxyz.com"
+
+exit 0
