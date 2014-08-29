4347212f96b3502b95b3eda8f8827a3597d31879
Andrew Stevens <andxyz.stevens@gmail.com>
2014-08-29 14:41:44 -0400

    Updated my avatar, due to peer pressure. haha

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-git push origin andxyz > /dev/null 2>&1 && 
+git push origin andxyz > /dev/null 2>&1 &&
 echo "updated deployer@deployer.andxyz.com:andxyz-writings.git repo"
 
 git push github andxyz > /dev/null 2>&1 &&
 echo "updated git@github.com-andxyz:andxyz/andxyz-writings.git repo"
 
-bundle exec rake clean > /dev/null 2>&1 && 
-bundle exec rake generate > /dev/null 2>&1 && 
-bundle exec rake deploy > /dev/null 2>&1 && 
+bundle exec rake clean > /dev/null 2>&1 &&
+bundle exec rake generate > /dev/null 2>&1 &&
+bundle exec rake deploy > /dev/null 2>&1 &&
 echo "deployed to http://andxyz.com"
 
 exit 0
