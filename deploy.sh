e8a20f232bac09393c175c3bb950b27239c0138a
Andrew Stevens <andxyz.stevens@gmail.com>
2015-12-03 14:23:38 -0500

    fix deploy.sh command

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
+set -x
 set -e
 
-git push origin andxyz > /dev/null 2>&1 &&
+git push origin andxyz 2>&1 > /dev/null
 echo "updated deployer@deployer.andxyz.com:andxyz-writings.git repo"
 
-git push github andxyz > /dev/null 2>&1 &&
+git push github andxyz 2>&1 > /dev/null
 echo "updated git@github.com-andxyz:andxyz/andxyz-writings.git repo"
 
-bundle exec rake clean > /dev/null 2>&1 &&
-bundle exec rake generate > /dev/null 2>&1 &&
-bundle exec rake deploy > /dev/null 2>&1 &&
+bundle exec rake clean 2>&1 > /dev/null
+bundle exec rake generate 2>&1 > /dev/null
+bundle exec rake deploy 2>&1 > /dev/null
 echo "deployed to http://andxyz.com"
 
 exit 0
