a99b25c4de00e99a17baf8c016936e8318d9d674
Andrew Stevens <andxyz.stevens@gmail.com>
2018-02-17 03:48:59 -0500

    Massive move from octopress to current jekyll

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ echo "updated deployer@deployer.andxyz.com:andxyz-writings.git repo"
 git push github andxyz 2>&1 > /dev/null
 echo "updated git@github.com-andxyz:andxyz/andxyz-writings.git repo"
 
-bundle exec rake clean 2>&1 > /dev/null
-bundle exec rake generate 2>&1 > /dev/null
+bundle exec jekyll clean 2>&1 > /dev/null
+bundle exec jekyll generate 2>&1 > /dev/null
 bundle exec rake deploy 2>&1 > /dev/null
-echo "deployed to http://andxyz.com"
+echo "deployed to https://andxyz.com"
 
 exit 0
